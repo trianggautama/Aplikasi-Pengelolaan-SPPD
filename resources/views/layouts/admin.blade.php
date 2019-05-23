75e40c4e4faaf26dd5013f44fc8cf450387707e9
unknown <tomycules@gmail.com>
2019-05-24 02:05:34 +0800

    enable kelurahan link

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -77,7 +77,7 @@
             <a class="collapse-item" href="{{ route('provinsi_index') }}">Data Provinsi</a>
             <a class="collapse-item" href="{{ route('kabupaten_index') }}">Data Kabupaten / Kota</a>
             <a class="collapse-item" href="{{ route('kecamatan_index') }}">Data Kecamatan</a>
-            {{-- <a class="collapse-item" href="{{ route('kelurahan_index') }}">Data Kelurahan</a> --}}
+            <a class="collapse-item" href="{{ route('kelurahan_index') }}">Data Kelurahan</a>
           </div>
         </div>
       </li>
