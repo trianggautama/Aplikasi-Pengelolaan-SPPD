b957f94851894400bf37636079dfad9cda50cb37
unknown <tomycules@gmail.com>
2019-06-09 13:50:22 +0800

    add kecamatan relation

--- app/Kabupaten.php
+++ app/Kabupaten.php
@@ -6,11 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kabupaten extends Model
 {
-    public function provinsi(){
-        return $this->belongsTo('App\provinsi', 'id_provinsi');
-      }
-      
+    
     public function kecamatan(){
-        return $this->hasMany('App\kecamatan');
+        return $this->hasMany('App\kecamatan','id_kabupaten');
       }
 }
