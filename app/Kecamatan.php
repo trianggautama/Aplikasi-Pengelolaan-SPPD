acf0d7b3f7dec6266a75b5a3964b4817599310f4
unknown <tomycules@gmail.com>
2019-06-09 13:50:58 +0800

    delete uneeded function

--- app/Kecamatan.php
+++ app/Kecamatan.php
@@ -9,4 +9,8 @@ class Kecamatan extends Model
     public function kabupaten(){
         return $this->belongsTo('App\kabupaten', 'id_kabupaten');
       }
+
+    public function kelurahan(){
+        return $this->hasMany('App\kelurahan','id_kecamatan');
+      }  
 }
