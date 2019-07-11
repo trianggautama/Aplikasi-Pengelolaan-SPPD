0e1faaa1c054298e3440a75d74c82f61a3fcac7f
Toms <toms@Tomss-MacBook-Pro.local>
2019-07-11 22:06:31 +0800

    sppd model

--- app/Sppd.php
+++ app/Sppd.php
@@ -10,30 +10,30 @@ class Sppd extends Model
     protected $fillable = [
         'kode_sppd','karyawan_id','anggaran_id','kegiatan_id','tgl_berangkat','tgl_kembali',
     ];
-    
+
     public function karyawan(){
         return $this->belongsTo('App\karyawan', 'karyawan_id');
       }
-    
+
     public function anggaran(){
         return $this->belongsTo('App\anggaran', 'anggaran_id');
-      }  
-     
+      }
+
     public function kegiatan(){
         return $this->belongsTo('App\kegiatan', 'kegiatan_id');
-      }    
+      }
 
     public function tujuan(){
         return $this->belongsTo('App\tujuan', 'tujuan_id');
-      }      
+      }
 
     public function transportasi(){
         return $this->belongsTo('App\transportasi', 'transportasi_id');
-      }    
-      
+      }
+
     public function pejabat(){
         return $this->belongsTo('App\pejabat', 'pejabat_id');
-      }      
+      }
 }
 
 
